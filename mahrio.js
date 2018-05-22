afc952247a7721126983930ff46c17e23649f971
Erik <emcp@whichdegree.co>
2018-05-22 21:00:09 +0200

    Upgrade to MAHRIO 1.5
    Required update in order to disable the TLS upgrade

--- mahrio.js
+++ mahrio.js
@@ -1,5 +1,6 @@
 process.env.NODE_URL = '10.10.47.200';
 process.env.NODE_PUBLIC_PATH = './';
+process.env.NODE_ENV = "development";
 
 require('mahrio').runServer(process.env, __dirname).then(function (server) {
   var io = require('socket.io').listen(server.listener);
